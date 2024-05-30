ba335921fb8e4a4002103c931e4e017d68f3f622
VampXDW <168702994+VampXDW@users.noreply.github.com>
2024-05-31 05:52:18 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,51 +1,27 @@
-const http = require("http");
-const fs = require("fs");
-const WebSocket = require("ws");
-const cluster = require("cluster");
-const os = require("os");
+const express = require('express');
+const http = require('http');
 
-const cpus = os.cpus().length;
-const port = 8080;
-const index = fs.readFileSync("./index.html");
+const app = express();
 
-if (cluster.isMaster) {
-  console.log(`Number of CPUs is ${cpus}`);
-  console.log(`Master ${process.pid} is running`);
+let totalReqCount = 0;
 
-  let requests = 0;
-  let childs = [];
-  for (let i = 0; i < cpus; i++) {
-    let child = cluster.fork();
-    child.on("message", (msg) => {
-      requests++;
-    });
-    childs.push(child);
-  }
+app.use((req, res, next) => {
+    totalReqCount++;
+    next();
+});
 
-  setInterval(() => {
-    for (let child of childs) {
-      child.send(requests);
-    }
-    requests = 0;
-  }, 1000);
-} else {
-  console.log(`Worker ${process.pid} started`);
+app.get('/api/totalReqCount', (req, res) => {
+    res.json({ totalReqCount });
+    totalReqCount = 0; 
+});
 
-  const handler = function (req, res) {
-    if (req.url == "/dstat") {
-      process.send(0);
-      res.end();
-    } else {
-      res.end(index);
-    }
-  };
+app.use(express.static('public'));
 
-  const server = http.createServer(handler);
-  const wss = new WebSocket.Server({ server });
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
 
-  process.on("message", (requests) => {
-    wss.clients.forEach((client) => client.send(requests));
-  });
-
-  server.listen(port);
-}
+const PORT = process.env.PORT || 8082;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
